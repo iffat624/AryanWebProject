57ec312698e9b79b8fe5fb8a7dda47bc02f6e9b2
Muteen Nabi Kundangar <kundangarmuteen@gmail.com>
2023-06-03 18:57:37 +0530

    Fixed Accordion JavaScript

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 const wrappers = document.querySelectorAll(".accordion-item-wrapper");
+const wrapperBodies = document.querySelectorAll(".acordion-body");
+const accordionIcons = document.querySelectorAll(".accordion-icon");
 
-
-
-wrappers.forEach((e) => {
+wrappers.forEach((e,i) => {
     e.addEventListener("click", () => {
-        console.log("helo");
+        let wrapperBody = wrapperBodies[i];
+        let accordionIcon = accordionIcons[i];
+        if (wrapperBody.classList.contains("accordion-body-closed")){
+            wrapperBody.classList.remove("accordion-body-closed");
+            wrapperBody.classList.add("accordion-body-open")
+            accordionIcon.style.transform = "rotate(180deg)";
+        }
+        else{
+            wrapperBody.classList.remove("accordion-body-open");
+            wrapperBody.classList.add("accordion-body-closed")
+            accordionIcon.style.transform = "rotate(0deg)";
+        }
     })
 })
